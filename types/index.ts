d2808447512b235dce8f0493aa6573028009cce9
Lore1ei <harubator@gmail.com>
2023-09-11 21:26:06 +0700

    first

---
+++ types/index.ts
@@ -0,0 +1,48 @@
+import {MouseEventHandler} from "react";
+
+export interface CustomButtonProps {
+    isDisabled?: boolean;
+    btnType?: "button" | "submit";
+    containerStyles?: string;
+    textStyles?: string;
+    title: string;
+    rightIcon?: string;
+    handleClick?: MouseEventHandler<HTMLButtonElement>;
+}
+
+export interface CarCard {
+    city_mpg: number;
+    class: string;
+    combination_mpg: number;
+    cylinders: number;
+    displacement: number;
+    drive: string;
+    fuel_type: string;
+    highway_mpg: number;
+    make: string;
+    model: string;
+    transmission: string;
+    year: number;
+}
+
+export interface FilterProps {
+    manufacturer?: string;
+    year?: number;
+    model?: string;
+    limit?: number;
+    fuel?: string;
+}
+
+export interface HomeProps {
+    searchParams: FilterProps;
+}
+
+export interface OptionProps{
+    title: string;
+    value: string;
+}
+
+export interface CustomFilters{
+    title: string;
+    options: OptionProps[]
+}
---
+++ utils/index.ts
@@ -0,0 +1,56 @@
+import {FilterProps} from "@/types";
+
+export async function fetchCars(filters: FilterProps){
+    const { manufacturer, year, model, limit, fuel } = filters;
+    const headers =  {
+            'X-RapidAPI-Key': 'a5fb77fce1msh6a312a33a7cb840p1a215ajsn8b1857f35e7e',
+            'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
+    }
+
+    const response = await fetch( `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`, {
+        headers: headers
+    })
+
+    return await response.json();
+}
+
+export const calculateCarRent = (city_mpg: number, year: number) => {
+    const basePricePerDay = 50; // Base rental price per day in dollars
+    const mileageFactor = 0.1; // Additional rate per mile driven
+    const ageFactor = 0.05; // Additional rate per year of vehicle age
+
+    // Calculate additional rate based on mileage and age
+    const mileageRate = city_mpg * mileageFactor;
+    const ageRate = (new Date().getFullYear() - year) * ageFactor;
+
+    // Calculate total rental rate per day
+    const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
+
+    return rentalRatePerDay.toFixed(0);
+};
+
+export const updateSearchParams = (type: string, value: string) => {
+    // Get the current URL search params
+    const searchParams = new URLSearchParams(window.location.search);
+
+    // Set the specified search parameter to the given value
+    searchParams.set(type, value);
+
+    // Set the specified search parameter to the given value
+    return `${window.location.pathname}?${searchParams.toString()}`;
+};
+
+// export const generateCarImageUrl = (car: CarCard, angle?: string) => {
+//     const url = new URL("https://cdn.imagin.studio/getimage");
+//     const { make, model, year } = car;
+//
+//     url.searchParams.append('customer', process.env.NEXT_PUBLIC_IMAGIN_API_KEY || 'hrjavascript-mastery');
+//     url.searchParams.append('make', make);
+//     url.searchParams.append('modelFamily', model.split(" ")[0]);
+//     url.searchParams.append('zoomType', 'fullscreen');
+//     url.searchParams.append('modelYear', `${year}`);
+//     // url.searchParams.append('zoomLevel', zoomLevel);
+//     url.searchParams.append('angle', `${angle}`);
+//
+//     return `${url}`;
+// }
